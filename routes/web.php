534260514e2df9e892fec7c3ef6e278da4214432
Muhammad Shahbaz <mrshahbaznnsd@gmail.com>
2025-11-30 11:09:02 +0500

    Add PreventAccessFromTenantDomains middleware to web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\PreventAccessFromTenantDomains;
 
-Route::get('/', function () {
-    return '<h1>Welcome to Central Domain (cip-tools.de)</h1>';
+// Ye middleware ensure karega ke ye routes sirf Central Domain par hi khulein
+Route::middleware([
+    'web',
+    PreventAccessFromTenantDomains::class,
+])->group(function () {
+
+    Route::get('/', function () {
+        return '<h1>Welcome to Central Domain (cip-tools.de)</h1>';
+    });
+
 });
