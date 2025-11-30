babcf34c22b0666370affd21e0fe12893baa406b
Muhammad Shahbaz <mrshahbaznnsd@gmail.com>
2025-11-30 11:00:04 +0500

    Add trial fields migration

---
+++ database/migrations/2025_11_30_055924_add_trial_fields_to_tenants_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tenants', function (Blueprint $table) {
+            $table->date('trial_ends_at')->nullable();
+            $table->string('plan_status')->default('trial'); // values: trial, active, expired
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tenants', function (Blueprint $table) {
+            //
+        });
+    }
+};
