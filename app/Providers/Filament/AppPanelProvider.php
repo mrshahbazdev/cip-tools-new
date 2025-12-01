909db09059088159e4627a8b38571d1fc97f1fc3
Muhammad Shahbaz <mrshahbaznnsd@gmail.com>
2025-12-01 12:18:52 +0500

    Add is_admin column for scaling super admin role

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -57,8 +57,8 @@ class AppPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                //Pages\Dashboard::class,
-                \App\Filament\Pages\TenantDashboard::class,
+                Pages\Dashboard::class,
+                //\App\Filament\Pages\TenantDashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
