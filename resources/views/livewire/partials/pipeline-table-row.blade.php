f0cddd1b26d2640fbc7a4ab17edb272dce637e6d
Muhammad Shahbaz <mrshahbaznnsd@gmail.com>
2025-12-04 21:55:23 +0500

    Final Fix: Removed conflicting wire:model bindings from Pipeline row and added correct value attribute

--- resources/views/livewire/partials/pipeline-table-row.blade.php
+++ resources/views/livewire/partials/pipeline-table-row.blade.php
@@ -21,9 +21,11 @@
 
     <td class="px-6 py-4 whitespace-nowrap">
         <div class="text-sm font-medium text-gray-900">{{ $idea->problem_short }}</div>
+        
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
         <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full {{ $statusBg }} mt-1">{{ $idea->status }}</span>
     </td>
-    
     <td class="px-6 py-4 whitespace-nowrap text-sm text-yellow-700 bg-yellow-50/50">
         {{ $idea->pain_score ?? 'N/A' }}
     </td>
