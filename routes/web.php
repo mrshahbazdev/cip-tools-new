b7a9f13f82d5108faddeffe50cccd0c56dcea22a
Muhammad Shahbaz <mrshahbaznnsd@gmail.com>
2025-12-05 10:18:17 +0500

    Resolved merge conflicts

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Sabse pehle, ensure karein ke wo DEBUGGING BLOCK poora HATA diya gaya hai.
 
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function () {
@@ -17,7 +16,3 @@ foreach (config('tenancy.central_domains') as $domain) {
         });
     });
 }
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
